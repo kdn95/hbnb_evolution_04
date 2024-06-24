ed29ce0d222695de390b5c58a27903c3ca35d984
Adrian <dreamweaver3d@gmail.com>
2024-06-24 14:10:51 +0800

    slight touch up

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,5 @@ import { HbnbHeader } from "../components/header.js"
 import { HbnbSearchForm } from "../components/search-form.js"
 import { HbnbFooter } from "../components/footer.js"
 
-// TODO: load stuff here
-
 // Hide the loader
 document.getElementById("loader").setAttribute('class', 'hide')
