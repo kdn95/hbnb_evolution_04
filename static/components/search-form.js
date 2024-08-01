aff738f201b3e120701647809235760b10c2587f
NickelannG <8836@holbertonstudents.com>
2024-08-01 16:36:11 +1000

    added hero section

--- static/components/search-form.js
+++ static/components/search-form.js
@@ -21,6 +21,14 @@ export class HbnbSearchForm extends HTMLElement {
 
         // --- HTML ---
         this.innerHTML = `
+            <section class="section-hero">
+                <section class="hero-inner">
+                    <h2 class="hero-title">Book the best hotel for your perfect getaway</h2>
+                </section>
+                <section class="hero-outer">
+                    <h6 class="hero-para">Welcome to the HBnB accommodation booking site. Explore and book accommodation in a variety of different locations. Find your perfect stay with our amenity filters - It’s that easy!</h6>
+                </section>
+            </section>
             <div class="none">
                 <h1>Start searching!</h1>
             </div>
