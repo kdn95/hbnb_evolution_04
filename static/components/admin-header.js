85eb4ed32448fe6ec6a6a3d0bed8b50a4534ef97
kdn95 <khangdn95@gmail.com>
2024-08-02 12:51:19 +1000

    updated index sass & added script tag into admin.html

--- static/components/admin-header.js
+++ static/components/admin-header.js
@@ -7,8 +7,8 @@ export class HbnbAdminHeader extends HTMLElement {
     connectedCallback() {
         // const shadow = this.attachShadow({ mode: "closed" });
         this.innerHTML = `
-            <div class="content">
-                <div class="title">Admin Page</div>
+            <div class="admin-content">
+                <div class="admin-title">Admin Page</div>
             </div>
         `;
     }
